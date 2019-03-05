db88b8a98f58b57f9175aab32721504b9d4a08bb
colour10 <84299915@qq.com>
2019-03-05 17:12:20 +0800

    修改表结构

--- app/api/controllers/UserController.php
+++ app/api/controllers/UserController.php
@@ -1,13 +1,28 @@
 <?php
 
+/**
+ * 用户操作类
+ */
+
 namespace Multiple\Api\Controllers;
 
 use Phalcon\Mvc\Controller;
 
 class UserController extends Controller
 {
+    /**
+     * 判断是否登录
+     */
     public function isloginAction()
     {
-        echo 'islogin';
+        echo 'ok';
+    }
+
+    public function loginAction()
+    {
+        if ($this->request->isPost()) {
+            echo 'post提交';
+        }
+        echo '没有任何提交';
     }
 }
