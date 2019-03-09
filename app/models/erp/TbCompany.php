acc0d6d81877a1ca1123ee5e1aa0db869c0222f5
colour10 <84299915@qq.com>
2019-03-09 17:44:35 +0800

    添加公司表验证规则

--- app/models/erp/TbCompany.php
+++ app/models/erp/TbCompany.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace Asa\Erp;
+
 use Phalcon\Validation;
-use Phalcon\Validation\Validator\Between;
 use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\Regex;
 
 /**
  * 公司表
@@ -15,28 +18,61 @@ class TbCompany extends BaseModel
         $this->setSource('tb_company');
     }
 
-    public function validation() {
+    /**
+     * 验证器
+     * @return bool
+     */
+    public function validation()
+    {
         $validator = new Validation();
 
-//        $validator->add(
-//            "age",
-//            new Between(
-//                [
-//                    "minimum" => 18,
-//                    "maximum" => 60,
-//                    "message" => "年龄必须是18~60岁",
-//                ]
-//            )
-//        );
-//
-//        $validator->add(
-//            'name',
-//            new Uniqueness(
-//                [
-//                    'message' => '姓名不能重复',
-//                ]
-//            )
-//        );
+        // name-公司名称不能为空或者重复
+        $validator->add('name', new PresenceOf([
+            'message' => 'The name is required',
+            'cancelOnFail' => true,
+        ]));
+        $validator->add('name', new Uniqueness([
+            'message' => 'The name field must be unique',
+            'cancelOnFail' => true,
+        ]));
+        // countryid-所属国家ID
+        $validator->add('countryid', new Regex(
+            [
+                "message" => "The countryid is invalid",
+                "pattern" => "/[0-9]+/",
+                "allowEmpty" => true,
+                'cancelOnFail' => true,
+            ]
+        ));
+        // relateid-关联ID
+        $validator->add('relateid', new Regex(
+            [
+                "message" => "The relateid is invalid",
+                "pattern" => "/[0-9]+/",
+                "allowEmpty" => true,
+                'cancelOnFail' => true,
+            ]
+        ));
+        // lang_code-语言编码
+        $validator->add('lang_code', new Regex(
+            [
+                "message" => "The lang_code is invalid",
+                "pattern" => "/[a-zA-Z\-_0-9]+/",
+                "allowEmpty" => true,
+                'cancelOnFail' => true,
+            ]
+        ));
+        // languages-语言列表
+        $validator->add('languages', new Regex(
+            [
+                "message" => "The languages is invalid",
+                "pattern" => "/[a-zA-Z0-9\-\_\,]+/",
+                "allowEmpty" => true,
+                'cancelOnFail' => true,
+            ]
+        ));
+        // 过滤
+        $validator->setFilters('name', 'trim');
 
         return $this->validate($validator);
     }
