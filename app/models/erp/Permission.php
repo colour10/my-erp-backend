48c9d30e1237361d0bd75c4b39e40aad753d5eab
colour10 <84299915@qq.com>
2019-03-09 09:54:27 +0800

    加入权限验证规则

--- app/models/erp/Permission.php
+++ app/models/erp/Permission.php
@@ -1,42 +1,39 @@
 <?php
+
 namespace Asa\Erp;
+
 use Phalcon\Validation;
-use Phalcon\Validation\Validator\Between;
 use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\PresenceOf;
 
 /**
  * 权限表
  */
 class Permission extends BaseModel
 {
+    /**
+     * 初始化
+     */
     public function initialize()
     {
         parent::initialize();
         $this->setSource('permission');
     }
 
+    /**
+     * 验证器
+     * @return bool
+     */
     public function validation() {
         $validator = new Validation();
 
-//        $validator->add(
-//            "age",
-//            new Between(
-//                [
-//                    "minimum" => 18,
-//                    "maximum" => 60,
-//                    "message" => "年龄必须是18~60岁",
-//                ]
-//            )
-//        );
-//
-//        $validator->add(
-//            'name',
-//            new Uniqueness(
-//                [
-//                    'message' => '姓名不能重复',
-//                ]
-//            )
-//        );
+        // 验证权限名称不能为空或者重复
+        $validator->add('name', new PresenceOf([
+            'message' => '权限名称不能为空',
+        ]));
+        $validator->add('name', new Uniqueness([
+            'message' => '权限名称不能重复',
+        ]));
 
         return $this->validate($validator);
     }
