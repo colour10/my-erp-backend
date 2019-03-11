66780644f4a1b10cda5ac249a7d6a250d1664b87
colour10 <84299915@qq.com>
2019-03-11 14:46:54 +0800

    新加一个部门一维数组展示

--- app/models/erp/Util.php
+++ app/models/erp/Util.php
@@ -44,4 +44,35 @@ class Util {
         // 返回
         return $tree;
     }
+
+    /**
+     * 格式化为目录树-一维数组
+     * @param $result 结果集
+     * @param int $pid 上级id
+     * @param int $level 上级等级
+     * @return array
+     */
+    public static function format_tree_single_array($result, $pid = 0, $level = 0)
+    {
+        // 初始化变量
+        $tree = [];
+        $str_repeat = ' ';
+        // 开始循环
+        foreach ($result as $k => $v) {
+            // 找到父级是0的
+            if ($v['up_dp_id'] == $pid) {
+                // 数据合并
+                    $tree[] = [
+                        'id' => $v['id'],
+                        'label' => str_repeat($str_repeat, $level).$v['name'],
+                        'remark' => $v['remark'],
+                        'level' => $level,
+                    ];
+                    // 继续寻找
+                    $tree = array_merge($tree, self::format_tree_single_array($result, $v['id'], $level+1));
+                }
+            }
+            // 返回
+            return $tree;
+        }
 }
