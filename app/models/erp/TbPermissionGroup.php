f27ba3e0aebdd5e5130a28c7e80164931ed6d63b
colour10 <84299915@qq.com>
2019-03-14 18:07:26 +0800

    增加软删除

--- app/models/erp/TbPermissionGroup.php
+++ app/models/erp/TbPermissionGroup.php
@@ -4,6 +4,7 @@ namespace Asa\Erp;
 
 use Phalcon\Validation;
 use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Mvc\Model\Relation;
 
 /**
  * 权限-组模型
@@ -26,6 +27,11 @@ class TbPermissionGroup extends BaseModel
             "id",
             [
                 'alias' => 'group',
+                'foreignKey' => [
+                    // 关联字段禁止自动删除
+                    'action' => Relation::ACTION_RESTRICT,
+                    "message"    => "The groupid does not exist on the group model"
+                ],
             ]
         );
 
@@ -36,6 +42,11 @@ class TbPermissionGroup extends BaseModel
             "id",
             [
                 'alias' => 'permission',
+                'foreignKey' => [
+                    // 关联字段禁止自动删除
+                    'action' => Relation::ACTION_RESTRICT,
+                    "message"    => "The permissionid does not exist on the permission model"
+                ],
             ]
         );
 
@@ -46,6 +57,11 @@ class TbPermissionGroup extends BaseModel
             'permissionid',
             [
                 'alias' => 'modules',
+                'foreignKey' => [
+                    // 关联字段禁止自动删除
+                    'action' => Relation::ACTION_RESTRICT,
+                    "message"    => "The permissiongroup cannot be deleted because other modules are using it"
+                ],
             ]
         );
     }
