db1f4363d061ae5d8d2f07f2efb94b29d3663403
colour10 <84299915@qq.com>
2019-03-16 11:48:13 +0800

    年代增加验证

--- app/models/erp/ZlAgeseason.php
+++ app/models/erp/ZlAgeseason.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Asa\Erp;
+
 use Phalcon\Validation;
-use Phalcon\Validation\Validator\Between;
-use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\Regex;
+use Phalcon\Validation\Validator\PresenceOf;
 
 /**
  * 年代季节
@@ -14,30 +16,57 @@ class ZlAgeseason extends BaseModel
         parent::initialize();
         $this->setSource('zl_ageseason');
     }
-    
-    public function validation() {
+
+
+    /**
+     * 验证器
+     * @return bool
+     */
+    public function validation()
+    {
         $validator = new Validation();
 
-//        $validator->add(
-//            "age",
-//            new Between(
-//                [
-//                    "minimum" => 18,
-//                    "maximum" => 60,
-//                    "message" => "年龄必须是18~60岁",
-//                ]
-//            )
-//        );
-//        
-//        $validator->add(
-//            'name',
-//            new Uniqueness(
-//                [
-//                    'message' => '姓名不能重复',
-//                ]
-//            )
-//        );
+        // name-年代数字不能为空
+        $validator->add('name', new PresenceOf([
+            'message' => $this->getValidateMessage('required', 'ageseason-name'),
+            'cancelOnFail' => true,
+        ]));
+        $validator->add('name', new Regex(
+            [
+                'message' => $this->getValidateMessage('invalid', 'ageseason-name'),
+                "pattern" => "/^[1-9]\d*$/",
+                'cancelOnFail' => true,
+            ]
+        ));
+
+        // mark-年代英文标识不能为空
+        $validator->add('mark', new PresenceOf([
+            'message' => $this->getValidateMessage('required', 'ageseason-mark'),
+            'cancelOnFail' => true,
+        ]));
+
+        // 过滤
+        $validator->setFilters('name', 'trim');
+        $validator->setFilters('mark', 'trim');
 
         return $this->validate($validator);
+    }
+
+    /**
+     * 重写多语言版本配置读取函数
+     * @param languages下面语言文件字段的名称 如template模块下面的uniqueness
+     * @param 待验证字段的编号，显示为当前语言的友好性提示 $name
+     * @return string
+     */
+    public function getValidateMessage($template, $name)
+    {
+        // 定义变量
+        // 取出当前语言版本
+        $language = $this->getDI()->get('language');
+        // 拼接变量
+        $template_name = $language->template[$template];
+        $human_name = $language->$name;
+        // 返回最终的友好提示信息
+        return sprintf($template_name, $human_name);
     }
 }
