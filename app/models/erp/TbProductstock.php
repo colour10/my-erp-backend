d587b40bb919c7911585a0ca7fe6d7b34d2c7a17
colour10 <84299915@qq.com>
2019-03-21 17:30:31 +0800

    添加模型关联

--- app/models/erp/TbProductstock.php
+++ app/models/erp/TbProductstock.php
@@ -1,8 +1,12 @@
 <?php
+
 namespace Asa\Erp;
+
 use Phalcon\Validation;
-use Phalcon\Validation\Validator\Between;
 use Phalcon\Validation\Validator\Uniqueness;
+use Phalcon\Validation\Validator\PresenceOf;
+use Phalcon\Validation\Validator\Regex;
+use Phalcon\Mvc\Model\Relation;
 
 /**
  * 库存表
@@ -13,31 +17,116 @@ class TbProductstock extends BaseModel
     {
         parent::initialize();
         $this->setSource('tb_productstock');
+
+        // 库存-商品主表，一对多反向
+        $this->belongsTo(
+            'productid',
+            '\Asa\Erp\TbProduct',
+            'id',
+            [
+                'alias' => 'product',
+                "foreignKey" => [
+                    // 关联字段存在性验证
+                    'action' => Relation::ACTION_RESTRICT,
+                    "message" => $this->getValidateMessage('notexist', 'product'),
+                ],
+            ]
+        );
+
+        // 库存-尺码表，一对多反向
+        $this->belongsTo(
+            'sizecontentid',
+            '\Asa\Erp\ZlSizecontent',
+            'id',
+            [
+                'alias' => 'sizecontent',
+                "foreignKey" => [
+                    // 关联字段存在性验证
+                    'action' => Relation::ACTION_RESTRICT,
+                    "message" => $this->getValidateMessage('notexist', 'sizecontent'),
+                ],
+            ]
+        );
+
+        // 库存-仓库表，一对多反向
+        $this->belongsTo(
+            'warehouseid',
+            '\Asa\Erp\TbWarehouse',
+            'id',
+            [
+                'alias' => 'warehouse',
+                "foreignKey" => [
+                    // 关联字段存在性验证
+                    'action' => Relation::ACTION_RESTRICT,
+                    "message" => $this->getValidateMessage('notexist', 'warehouse'),
+                ],
+            ]
+        );
     }
 
-    public function validation() {
+    /**
+     * 验证器
+     * @return bool
+     */
+    public function validation()
+    {
         $validator = new Validation();
 
-//        $validator->add(
-//            "age",
-//            new Between(
-//                [
-//                    "minimum" => 18,
-//                    "maximum" => 60,
-//                    "message" => "年龄必须是18~60岁",
-//                ]
-//            )
-//        );
-//
-//        $validator->add(
-//            'name',
-//            new Uniqueness(
-//                [
-//                    'message' => '姓名不能重复',
-//                ]
-//            )
-//        );
+        // productid-商品编号不能为空
+        $validator->add('productid', new PresenceOf([
+            'message' => $this->getValidateMessage('required', 'productid'),
+            'cancelOnFail' => true,
+        ]));
+
+        // number-商品库存数量不能为空
+        $validator->add('number', new PresenceOf([
+            'message' => $this->getValidateMessage('required', 'number'),
+            'cancelOnFail' => true,
+        ]));
+
+        // number-商品库存数量不能为空
+        $validator->add('number', new Regex([
+            'message' => $this->getValidateMessage('invalid', 'number'),
+            "pattern" => "/^[0-9]+$/",
+            'cancelOnFail' => true,
+        ]));
+
+        // sizecontentid-尺码编号不能为空
+        $validator->add('sizecontentid', new PresenceOf([
+            'message' => $this->getValidateMessage('required', 'sizecontentid'),
+            'cancelOnFail' => true,
+        ]));
+
+        // warehouseid-仓库编号不能为空
+        $validator->add('warehouseid', new PresenceOf([
+            'message' => $this->getValidateMessage('required', 'warehouseid'),
+            'cancelOnFail' => true,
+        ]));
+
+        // 商品id，尺码id，仓库id建立唯一性索引
+        $validator->add(['productid', 'sizecontentid', 'warehouseid'], new Uniqueness([
+            'message' => $this->getValidateMessage('uniqueness', 'productstock-productid-sizecontentid-warehouseid'),
+            'cancelOnFail' => true,
+        ]));
 
         return $this->validate($validator);
+    }
+
+    /**
+     * 重写多语言版本配置读取函数
+     * @param languages下面语言文件字段的名称 如template模块下面的uniqueness
+     * @param 待验证字段的编号，显示为当前语言的友好性提示 $name
+     * @return string
+     */
+    public function getValidateMessage($template, $name)
+    {
+        // 定义变量
+        // 取出当前语言版本
+        $language = $this->getDI()->get('language');
+        // 拼接变量
+        $template_name = $language->template[$template];
+        $human_name = $language->$name;
+        // 返回最终的友好提示信息
+        return sprintf($template_name, $human_name);
     }
 }
