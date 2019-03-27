583ac6059d97656a6dcbe46442b1d11d7d88081d
colour10 <84299915@qq.com>
2019-03-27 09:42:00 +0800

    增加一个获取品类list的函数用于跨域

--- app/shop/controllers/BrandgroupController.php
+++ app/shop/controllers/BrandgroupController.php
@@ -1,27 +1,19 @@
 <?php
-namespace Multiple\Home\Controllers;
+namespace Multiple\Shop\Controllers;
 
 use Phalcon\Mvc\Controller;
 use Asa\Erp\ZlBrandgroup;
 
-class BrandgroupController extends ZadminController
+class BrandgroupController extends AdminController
 {
 
 	public function initialize()
     {
-        parent::initialize();
         $this->setModelName('Asa\\Erp\\ZlBrandgroup');
-        
-        // $this->setTitle("品类维护");
     }
 
-    /**
-     * 获取主品牌列表
-     */
-    public function getlistAction()
+    public function indexAction()
     {
-        // 逻辑
-        $list = ZlBrandgroup::find('sys_delete_flag=0');
-        return $this->success($list);
+
     }
 }
