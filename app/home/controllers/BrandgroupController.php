4e93c0e72f44a296e0c9964c2f97cbd9a32b2b47
colour10 <84299915@qq.com>
2019-03-27 19:43:57 +0800

    添加跨域函数

--- app/home/controllers/BrandgroupController.php
+++ app/home/controllers/BrandgroupController.php
@@ -12,6 +12,34 @@ class BrandgroupController extends ZadminController
         parent::initialize();
         $this->setModelName('Asa\\Erp\\ZlBrandgroup');
         
-        $this->setTitle("品类维护");
+        // $this->setTitle("品类维护");
+    }
+
+    /**
+     * 获取主品牌列表，跨域使用
+     * @return string
+     */
+    public function getlistAction()
+    {
+        // 逻辑
+        $callback = $this->request->get('callback');
+        $list = ZlBrandgroup::find();
+        // 此处的callback需要和客户端ajax请求中的jsonp属性保持一致。
+        return $callback."(".json_encode($list).")";
+    }
+
+
+    /**
+     * 获取当前品类的相关信息
+     * @param $id
+     * @return string
+     */
+    public function getdetailAction($id)
+    {
+        // 逻辑
+        $callback = $this->request->get('callback');
+        $brandGroup = ZlBrandgroup::find('id='.$id);
+        // 此处的callback需要和客户端ajax请求中的jsonp属性保持一致。
+        return $callback."(".json_encode($brandGroup).")";
     }
 }
