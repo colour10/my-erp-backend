499c631923d69a0c453198c5b9c7a49b21560602
colour10 <84299915@qq.com>
2019-03-29 18:23:29 +0800

    增加参数验证

--- app/shop/controllers/ChildproductgroupController.php
+++ app/shop/controllers/ChildproductgroupController.php
@@ -15,17 +15,23 @@ class ChildproductgroupController extends AdminController {
 	    $this->setModelName('Asa\\Erp\\ZlChildproductgroup');
     }
 
+
     /**
      * 获取当前子分类产品列表
-     * @param $id
-     * @return mixed
      */
-    public function detailAction($id)
+    public function detailAction()
     {
         // 逻辑
+        // 先过滤
+        $params = $this->dispatcher->getParams();
+        if (!$params || !preg_match('/^[1-9]+\d*$/', $params[0])) {
+            exit('Params error!');
+        }
+
+        // 赋值
+        $id = $params[0];
         // 分页
         $currentPage = $this->request->getQuery("page", "int", 1);
-
         // 取出数据
         $model = ZlChildproductgroup::findFirstById($id);
         $products= $model->products;
