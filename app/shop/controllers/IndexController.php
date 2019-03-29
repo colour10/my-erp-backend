683f2e804b1f499f701151a61e0af7b73096391b
colour10 <84299915@qq.com>
2019-03-29 17:13:39 +0800

    添加搜索功能

--- app/shop/controllers/IndexController.php
+++ app/shop/controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace Multiple\Shop\Controllers;
 
 use Asa\Erp\TbProduct;
+use Phalcon\Paginator\Adapter\Model as PaginatorModel;
 
 class IndexController extends AdminController
 {
@@ -22,14 +23,38 @@ class IndexController extends AdminController
     }
 
     /**
-     * 测试，如果需要引用其他命名空间的模块，只能用dispatcher来做跳转
+     * 搜索
      */
-    public function testAction()
+    public function searchAction()
     {
-        $this->dispatcher->forward([
-            'module' => 'home',
-            'controller' => 'index',
-            'action' => 'index'
+        // 逻辑
+        // 外部搜索内容
+        $keyword = $this->request->get('keyword', 'string', '');
+        // 分页
+        $currentPage = $this->request->getQuery("page", "int", 1);
+
+        // 取出结果
+        $productlist = TbProduct::find([
+            'conditions' => 'productname like :keyword:',
+            'bind' => ['keyword' => '%'.$keyword.'%'],
+        ]);
+
+        // 创建分页对象
+        $paginator = new PaginatorModel(
+            [
+                "data"  => $productlist,
+                "limit" => 10,
+                "page"  => $currentPage,
+            ]
+        );
+
+        // 展示分页
+        $page = $paginator->getPaginate();
+
+        // 分配到模板
+        $this->view->setVars([
+            'page' => $page,
+            'keyword' => $keyword,
         ]);
     }
 }
