9da897f9208e931c35b1954fdfc675c044b8e446
colour10 <84299915@qq.com>
2019-04-08 09:14:08 +0800

    加入登录简单样式调整

--- app/shop/controllers/LoginController.php
+++ app/shop/controllers/LoginController.php
@@ -20,22 +20,38 @@ class LoginController extends AdminController
     {
     	
     }
-    
+
+    /**
+     * 会员登录逻辑
+     * @return false|string
+     */
      function loginAction()
     {
+        // 采用post接收
         if($this->request->isPost()) {
-            //
-            $username = $this->request->getPost('login_name');
-            $password = $this->request->getPost('password');
+
+            // 接收变量
+            $username = $this->request->get('login_name');
+            $password = $this->request->get('password');
+
+            // 验证合法性
+            if (!$username || !$password) {
+                return json_encode(['code' => '200', 'messages' => ['用户名或密码不能为空']]);
+            }
+
+            // 加密
+            $new_password = md5($password);
+            // 查找记录
             $rs = TbMember::findFirst(array(
-            	"login_name = '$username' and password = '$password'"
+            	"login_name = '$username' and password = '$new_password'"
             ));
+
+            // 分别返回
             if($rs){
             	$this->session->set('member',$rs->toArray());
-            	echo json_encode(['code' => '200', 'auth' =>$this->session->get('member'), 'messages' => []]);
-            }
-            else {
-                echo json_encode(['code' => '200', 'messages' => ["登录失败。用户名或密码错误。"]]);
+            	return json_encode(['code' => '200', 'auth' =>$this->session->get('member'), 'messages' => []]);
+            } else {
+                return json_encode(['code' => '200', 'messages' => ["登录失败。用户名或密码错误。"]]);
             }
         }
     
