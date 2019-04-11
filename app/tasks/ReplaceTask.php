3e43ae8e9ac5b7fca8c52361ad8c890d30aeb4c5
colour10 <84299915@qq.com>
2019-04-11 17:59:59 +0800

    增加一个替换id的命令行工具

--- app/tasks/ReplaceTask.php
+++ app/tasks/ReplaceTask.php
@@ -1,82 +1,141 @@
 <?php
-/*
- * php cli task
+
+use Asa\Models\Temp\TempTbAliases;
+use Asa\Models\Temp\TempTbBrand;
+use Asa\Models\Temp\TempTbSeries;
+use Asa\Models\Temp\TempTbSeries2;
+use Asa\Models\Temp\TempTbSizecontent;
+
+/**
+ * 临时表替换
+ * Class ReplaceTask
  */
-use Asa\Erp\TbProduct;
-use Asa\Erp\TbCompany;
-use Asa\Erp\TbProductSearch;
-use Asa\Erp\Util;
-
-class CrontabTask extends \Phalcon\CLI\Task
+class ReplaceTask extends \Phalcon\CLI\Task
 {
+    /**
+     * 临时表替换真实数据
+     * @return false|string
+     */
     public function mainAction() {
-        $db = $this->db;
-        $update = 0;
-        $insert = 0;
+        // 逻辑
+        // 采用事务处理
+        $this->db->begin();
 
-        $companyList = TbCompany::find();
-        foreach($companyList as $company) {
-            if($company->saleportid>0) {
-                //获得公司的对应自营店铺的销售端口对象。然后查询这个销售端口的商品数据及库存数据
-                $saleport = $company->shopSaleport;
-
-                //库存商品的id及合计库存（多个仓库，多个尺码合计）
-                $product_sum_list = $saleport->getProductList();
-
-                $product_id_array = Util::recordListColumn($product_sum_list, 'productid');
-                $products = TbProduct::findByIdString($product_id_array, 'id');
-                //print_r($products->toArray());
-
-                $hashmap = Util::recordToHashtable($product_sum_list, 'productid', 'sumatory');
-
-                //查询已经存在的库存记录，这些需要更新
-                $rows = TbProductSearch::find(
-                    sprintf("companyid=%d and productid in(%s)", $company->id, implode(",", $product_id_array))
-                );
-                $exist_product_id_array = Util::recordListColumn($rows, 'productid');
-
-
-                //print_r($hashmap);
-                foreach($products as $product ){
-                    if(in_array($product->id, $exist_product_id_array)) {
-                        //更新
-                        $sql = sprintf(
-                            "UPDATE tb_product_search SET productname='%s', number=%d, sizetopid=%d, brandgroupid=%d, childbrand=%d, picture='%s', picture2='%s' WHERE companyid=%d and productid=%d",
-                            addslashes($product->productname),
-                            $hashmap[$product->id],
-                            $product->sizetopid,
-                            $product->brandgroupid,
-                            $product->childbrand,
-                            addslashes($product->picture),
-                            addslashes($product->picture2),
-                            $company->id,
-                            $product->id
-                        );
-                        $db->execute($sql);
-                    }
-                    else {
-                        //新增
-                        $sql = sprintf(
-                            "INSERT INTO tb_product_search SET productname='%s', number=%d, sizetopid=%d, brandgroupid=%d, childbrand=%d, picture='%s', picture2='%s', companyid=%d, productid=%d",
-                            addslashes($product->productname),
-                            $hashmap[$product->id],
-                            $product->sizetopid,
-                            $product->brandgroupid,
-                            $product->childbrand,
-                            addslashes($product->picture),
-                            addslashes($product->picture2),
-                            $company->id,
-                            $product->id
-                        );
-                        $db->execute($sql);
-                    }
-                }
-
-                //清除未在产品列表里的产品记录
-                $sql = sprintf("DELETE FROM tb_product_search WHERE companyid=%d and productid not in(%s)", $company->id, implode(",", $product_id_array));
-                $db->execute($sql);
+        // 首先生成所有品牌表的countryid
+        $brands = TempTbBrand::find();
+        // 遍历替换品牌表的countryid
+        foreach ($brands as $brand) {
+            // 写入countryid，如果不存在记做NULL
+            $country = $brand->country;
+            if (!$country) {
+                $countryid = NULL;
+            } else {
+                $countryid = $country->id;
+            }
+            // 写入countryid
+            $data = [
+                'countryid' => $countryid,
+            ];
+            if (!$brand->save($data)) {
+                // 回滚
+                $this->db->rollback();
+                // 报错
+                return json_encode(['code' => '200', 'messages' => ['生成品牌表的countryid出错了']]);
             }
         }
-        echo "同步完毕";
+
+        // 接着生成所有别名表的brandid
+        $aliases = TempTbAliases::find();
+        // 遍历替换别名表的brandid
+        foreach ($aliases as $alias) {
+            // 写入brandid，如果不存在记做NULL
+            $brand = $alias->brand;
+            if (!$brand) {
+                $brandid = NULL;
+            } else {
+                $brandid = $brand->id;
+            }
+            // 写入brandid
+            $data = [
+                'brandid' => $brandid,
+            ];
+            if (!$alias->save($data)) {
+                // 回滚
+                $this->db->rollback();
+                // 报错
+                return json_encode(['code' => '200', 'messages' => ['生成别名表的brandid出错了']]);
+            }
+        }
+
+        // 接着生成所有尺码明细表的topid
+        $sizecontents = TempTbSizecontent::find();
+        // 遍历替换尺码明细表的topid
+        foreach ($sizecontents as $sizecontent) {
+            // 写入topid，如果不存在记做NULL
+            $sizetop = $sizecontent->sizetop;
+            if (!$sizetop) {
+                $topid = NULL;
+            } else {
+                $topid = $sizetop->id;
+            }
+            $data = [
+                'topid' => $topid,
+            ];
+            if (!$sizecontent->save($data)) {
+                // 回滚
+                $this->db->rollback();
+                // 报错
+                return json_encode(['code' => '200', 'messages' => ['生成尺码明细表的topid出错了']]);
+            }
+        }
+
+        // 给系列表添加品牌id-brandid
+        $series = TempTbSeries::find();
+        // 遍历替换系列表的brandid
+        foreach ($series as $serie) {
+            // 写入brandid，如果不存在记做NULL
+            $brand = $serie->brand;
+            if (!$brand) {
+                $brandid = NULL;
+            } else {
+                $brandid = $brand->id;
+            }
+            $data = [
+                'brandid' => $brandid,
+            ];
+            if (!$serie->save($data)) {
+                // 回滚
+                $this->db->rollback();
+                // 报错
+                return json_encode(['code' => '200', 'messages' => ['生成系列表的brandid出错了']]);
+            }
+        }
+
+        // 给子系列表添加系列表id-seriesid
+        $serie2s = TempTbSeries2::find();
+        // 遍历替换系列表的seriesid
+        foreach ($serie2s as $serie2) {
+            // 写入seriesid，如果不存在记做NULL
+            $serie = $serie2->serie;
+            if (!$serie) {
+                $seriesid = NULL;
+            } else {
+                $seriesid = $serie->id;
+            }
+            $data = [
+                'seriesid' => $seriesid,
+            ];
+            if (!$serie2->save($data)) {
+                // 回滚
+                $this->db->rollback();
+                // 报错
+                return json_encode(['code' => '200', 'messages' => ['生成子系列表的seriesid出错了']]);
+            }
+        }
+
+        // 提交事务
+        $this->db->commit();
+        // 返回成功
+        echo json_encode(['code' => '200', 'messages' => ['操作成功']]);
     }
 }
