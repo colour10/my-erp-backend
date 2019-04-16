34f2aec05bf2cc702607384df1ddfa9b06492dd6
colour10 <84299915@qq.com>
2019-04-16 10:55:30 +0800

    简单适配了移动端，并微调逻辑

--- app/shop/controllers/LoginController.php
+++ app/shop/controllers/LoginController.php
@@ -37,32 +37,32 @@ class LoginController extends AdminController
     {
         // 采用post接收
         if($this->request->isPost()) {
-
             // 接收变量
             $username = $this->request->get('login_name');
             $password = $this->request->get('password');
 
             // 验证合法性
             if (!$username || !$password) {
-                return json_encode(['code' => '200', 'messages' => ['用户名或密码不能为空']]);
+                return $this->error(['用户名或密码不能为空']);
             }
 
-            // 加密
-            $new_password = md5($password);
             // 查找记录
-            $rs = TbMember::findFirst(array(
-            	"login_name = '$username' and password = '$new_password'"
-            ));
+            $rs = TbMember::findFirst([
+                "login_name = :username: and password = :password:",
+                'bind' => [
+                    'username' => $username,
+                    'password' => md5($password),
+                ],
+            ]);
 
             // 分别返回
             if($rs){
-            	$this->session->set('member',$rs->toArray());
-            	return json_encode(['code' => '200', 'auth' =>$this->session->get('member'), 'messages' => []]);
+                $this->session->set('member', $rs->toArray());
+                return json_encode(['code' => '200', 'auth' =>$this->session->get('member'), 'messages' => []]);
             } else {
-                return json_encode(['code' => '200', 'messages' => ["登录失败：用户名或密码错误。"]]);
+                return $this->error(['登录失败，用户名或密码错误！']);
             }
         }
-    
     }
 
     /**
