56252e47090dc018e1819d27c91a866af7cf0f20
changwei <nkchwfree@163.com>
2019-04-16 19:39:55 +0800

    客户管理增加开户行和发票管理

---
+++ app/home/controllers/SupplierbankController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Multiple\Home\Controllers;
+
+use Phalcon\Mvc\Controller;
+use Phalcon\Mvc\View;
+use Asa\Erp\TbSupplierBank;
+
+/**
+ * 分组表
+ */
+class SupplierbankController extends CadminController {
+    public function initialize() {
+        parent::initialize();
+
+        $this->setModelName('Asa\\Erp\\TbSupplierBank');
+    }
+
+    function before_add() {
+        $_POST['companyid'] = $this->companyid;
+    }
+
+    function before_edit($row) {
+        if(isset($_POST['companyid']) || isset($_POST['supplierid']) || $row->companyid!=$this->companyid) {
+            throw new \Exception("/1001/数据非法/");
+        }
+    }
+
+    function before_delete($row) {
+        if($row->companyid!=$this->companyid) {
+            throw new \Exception('/1002/数据非法/');
+        }
+    }
+
+    function before_page() {
+        $_POST['companyid'] = $this->companyid;
+    }
+}
--- app/models/erp/Util.php
+++ app/models/erp/Util.php
@@ -249,6 +249,8 @@ class Util {
             ['series2','page'],
             ['productmemo', 'page'],
             ['companyinvoice', 'page'],
+            ['supplierbank','page'],
+            ['supplierinvoice','page'],
             ['company', 'info']
         ];
     }
