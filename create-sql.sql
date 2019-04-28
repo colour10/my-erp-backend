70772a61ae678318dc5cb0293b5cd6a9a2283174
colour10 <84299915@qq.com>
2019-04-28 16:28:56 +0800

    微调了tb_buycar和tb_shoporder表

--- create-sql.sql
+++ create-sql.sql
@@ -147,15 +147,16 @@ CREATE TABLE `tb_buycar` (
                            `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键ID',
                            `product_id` int(10) UNSIGNED NOT NULL COMMENT '商品id',
                            `product_name` varchar(255) NOT NULL COMMENT '商品名称',
-                           `color_id` int(10) UNSIGNED NOT NULL COMMENT '颜色id',
-                           `color_name` varchar(64) NOT NULL COMMENT '颜色名称',
+                           `color_id` varchar(64) NULL COMMENT '颜色id',
+                           `color_name` varchar(64) NULL COMMENT '颜色名称',
                            `size_id` int(11) NOT NULL COMMENT '规格id',
                            `size_name` varchar(64) NOT NULL COMMENT '规格名称',
                            `member_id` int(10) UNSIGNED NOT NULL COMMENT '用户id',
                            `number` int(10) UNSIGNED NOT NULL DEFAULT '1' COMMENT '数量',
                            `price` decimal(9,2) NOT NULL COMMENT '价格',
                            `total_price` decimal(9,2) NOT NULL COMMENT '总价格',
-                           `picture` varchar(255) DEFAULT NULL COMMENT '商品预览图',
+                           `picture` varchar(255) DEFAULT NULL COMMENT '商品主图',
+                           `picture2` varchar(255) DEFAULT NULL COMMENT '商品附图',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='购物车';
 
@@ -1457,9 +1458,10 @@ CREATE TABLE `tb_shoporder` (
                               `price` decimal(9,2) NOT NULL COMMENT '价格',
                               `number` int(10) NOT NULL COMMENT '数量',
                               `total_price` decimal(9,2) NOT NULL COMMENT '此商品的总价格',
-                              `picture` varchar(255) DEFAULT NULL,
-                              `color_id` int(10) NOT NULL COMMENT '颜色id',
-                              `color_name` varchar(64) NOT NULL COMMENT '颜色名称',
+                              `picture` varchar(255) DEFAULT NULL COMMENT '商品主图',
+                              `picture2` varchar(255) DEFAULT NULL COMMENT '商品附图',
+                              `color_id` varchar(64) NULL COMMENT '颜色id',
+                              `color_name` varchar(64) NULL COMMENT '颜色名称',
                               `size_id` int(10) NOT NULL COMMENT '规格id',
                               `size_name` varchar(64) NOT NULL COMMENT '规格名称',
                               PRIMARY KEY (`id`)
@@ -1483,7 +1485,8 @@ CREATE TABLE `tb_shoporder_common` (
                                      `final_price` decimal(9,2) DEFAULT NULL COMMENT '成交价',
                                      `order_status` int(1) DEFAULT '1' COMMENT '订单状态(1未支付；2已支付；未完成；3已完成；4已取消；5退款中；6已退款)',
                                      `pay_style` int(1) DEFAULT '0' COMMENT '支付方式(0未支付；1微信；2支付宝)',
-                                     `create_time` datetime DEFAULT NULL COMMENT '发起时间',
+                                     `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                                     `expire_time` datetime DEFAULT NULL COMMENT '库存锁定截止时间，默认为1个小时',
                                      `pay_time` datetime DEFAULT NULL COMMENT '支付时间',
                                      PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='支付订单主表';
