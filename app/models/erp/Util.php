ebbf3154b9eade46ff333e9c10e2e41cc82aed86
colour10 <84299915@qq.com>
2019-05-13 09:02:05 +0800

    增加一个新的函数diffBetweenTwoDays

--- app/models/erp/Util.php
+++ app/models/erp/Util.php
@@ -669,13 +669,34 @@ class Util
         }
     }
 
-    public static function filterIds($ids) {
+    public static function filterIds($ids)
+    {
         $array = explode(",", $ids);
 
         $result = [];
-        foreach($array as $row) {
+        foreach ($array as $row) {
             $result[] = (int)$row;
         }
         return implode(",", $result);
+    }
+
+    /**
+     * 求两个日期之间相差的天数
+     * (针对1970年1月1日之后，求之前可以采用泰勒公式)
+     * @param string $day1
+     * @param string $day2
+     * @return number
+     */
+    public static function diffBetweenTwoDays($day1, $day2)
+    {
+        $second1 = strtotime($day1);
+        $second2 = strtotime($day2);
+
+        if ($second1 < $second2) {
+            $tmp = $second2;
+            $second2 = $second1;
+            $second1 = $tmp;
+        }
+        return ($second1 - $second2) / 86400;
     }
 }
