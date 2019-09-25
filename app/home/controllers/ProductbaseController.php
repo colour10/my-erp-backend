01971e1c614d687731baafc77014413ff4626c53
colour10 <84299915@qq.com>
2019-09-25 15:20:18 +0800

    修改国际码下拉逻辑

--- app/home/controllers/ProductbaseController.php
+++ app/home/controllers/ProductbaseController.php
@@ -11,7 +11,7 @@ class ProductbaseController extends BaseController
 {
     /**
      * 实时下拉搜索框
-     * @return false|string
+     * @return false|string|void
      */
     public function suggestAction()
     {
@@ -21,7 +21,6 @@ class ProductbaseController extends BaseController
                     sprintf("wordcode like '%s%%'", addslashes($keyword)),
                     "order" => "wordcode asc",
                 ]);
-
                 $array = [];
                 foreach ($rows as $row) {
                     $array[] = $row->product->toArray();
