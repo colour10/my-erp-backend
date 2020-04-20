9d9a41ac452a8cc07a36cc4a1ac083b49645a73e
colour10 <84299915@qq.com>
2020-04-20 15:07:20 +0800

    去掉错误提示，防止有些notice级别的报错

--- public/index.php
+++ public/index.php
@@ -1,41 +1,39 @@
 <?php
 
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+use Phalcon\DI\FactoryDefault;
+use Phalcon\Events\Manager as EventsManager;
 use Phalcon\Loader;
-use Phalcon\Mvc\View;
+use Phalcon\Logger\Adapter\File as FileLogger;
 use Phalcon\Mvc\Application;
 use Phalcon\Mvc\Router;
-use Phalcon\DI\FactoryDefault;
-use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
 use Phalcon\Session\Adapter\Files as Session;
-use Phalcon\Events\Manager as EventsManager;
-use Phalcon\Logger;
-use Phalcon\Logger\Adapter\File as FileLogger;
 
 ini_set('date.timezone', 'Asia/Shanghai');
 
 try {
     define('APP_PATH', dirname(dirname(__FILE__)));
 
-    error_reporting(E_ALL);
-      ini_set('display_errors', 'On');
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', 'On');
 
     $config = new \Phalcon\Config\Adapter\Php(APP_PATH . "/app/config/config.php");
 
     // Register an autoloader
     $loader = new Loader();
     $loader->registerNamespaces(
-        array(
-            "Asa\Erp" => APP_PATH . '/app/models/erp',
+        [
+            "Asa\Erp"          => APP_PATH . '/app/models/erp',
             "Asa\Erp\Behavior" => APP_PATH . '/app/models/erp/behavior',
             // 新增测试临时表模型
-            "Asa\Models\Temp" => APP_PATH . '/app/models/temp',
-        )
+            "Asa\Models\Temp"  => APP_PATH . '/app/models/temp',
+        ]
     );
     $loader->registerDirs(
-        array(
+        [
             '../app/models/',
             '../app/plugins/',
-        )
+        ]
     )->register();
 
     //require_once(APP_PATH . "/app/models/vendor/autoload.php");
@@ -52,7 +50,7 @@ try {
     $di->setShared('session', function () {
         $session = new Session();
 
-        if(isset($_REQUEST["_session_id"])) {
+        if (isset($_REQUEST["_session_id"])) {
             $session->setId($_REQUEST["_session_id"]);
         }
 
@@ -64,10 +62,10 @@ try {
 
     // Set the database service
     $di['db'] = function () use ($config) {
-        if($config->mode=='develop') {
+        if ($config->mode == 'develop') {
             $eventsManager = new EventsManager();
 
-            $logger = new FileLogger($config->app->log_path . sprintf("/weblog_%s.log",date('Ymd')));
+            $logger = new FileLogger($config->app->log_path . sprintf("/weblog_%s.log", date('Ymd')));
 
             // Listen all the database events
             $eventsManager->attach('db', function ($event, $connection) use ($logger) {
@@ -82,8 +80,7 @@ try {
 
             $connection->setEventsManager($eventsManager);
             return $connection;
-        }
-        else {
+        } else {
 
             $connection = new DbAdapter($config->database->db->toArray());
 
@@ -92,119 +89,119 @@ try {
         }
     };
 
-    $di->set( "router", function () use ($config, $di){
-            $router = new Router();
+    $di->set("router", function () use ($config, $di) {
+        $router = new Router();
 
-            $router->setDefaultModule("home");
+        $router->setDefaultModule("home");
 
-            //ERP系统路由规则
-            $router->add(
-                "/",
-                [
-                    "module" => "home",
-                    "controller" => "index",
-                    "action" => "index"
-                ]
-            )->setHostName($config->app->main_host);
+        //ERP系统路由规则
+        $router->add(
+            "/",
+            [
+                "module"     => "home",
+                "controller" => "index",
+                "action"     => "index",
+            ]
+        )->setHostName($config->app->main_host);
 
-            $router->add(
-                "/:controller",
-                [
-                    "module" => "home",
-                    "controller" => 1,
-                    "action" => "index"
-                ]
-            )->setHostName($config->app->main_host);
+        $router->add(
+            "/:controller",
+            [
+                "module"     => "home",
+                "controller" => 1,
+                "action"     => "index",
+            ]
+        )->setHostName($config->app->main_host);
 
-            $router->add(
-                "/:controller/:action",
-                [
-                    "module" => "home",
-                    "controller" => 1,
-                    "action" => 2,
-                ]
-            )->setHostName($config->app->main_host);
+        $router->add(
+            "/:controller/:action",
+            [
+                "module"     => "home",
+                "controller" => 1,
+                "action"     => 2,
+            ]
+        )->setHostName($config->app->main_host);
 
-            $router->add(
-                "/l/([a-z]+)",
-                [
-                    "module" => "home",
-                    "controller" => "common",
-                    "action" => "list",
-                    "table" => 1
-                ]
-            )->setHostName($config->app->main_host);
+        $router->add(
+            "/l/([a-z]+)",
+            [
+                "module"     => "home",
+                "controller" => "common",
+                "action"     => "list",
+                "table"      => 1,
+            ]
+        )->setHostName($config->app->main_host);
 
 
-            // 店铺域名路由规则
-            $router->add(
-                "/",
-                [
-                    "module" => "shop",
-                    "controller" => "index",
-                    "action" => "index"
-                ]
-            )->setHostName($config->app->shop_host);
+        // 店铺域名路由规则
+        $router->add(
+            "/",
+            [
+                "module"     => "shop",
+                "controller" => "index",
+                "action"     => "index",
+            ]
+        )->setHostName($config->app->shop_host);
 
-            $router->add(
-                "/:controller",
-                [
-                    "module" => "shop",
-                    "controller" => 1,
-                    "action" => "index"
-                ]
-            )->setHostName($config->app->shop_host);
+        $router->add(
+            "/:controller",
+            [
+                "module"     => "shop",
+                "controller" => 1,
+                "action"     => "index",
+            ]
+        )->setHostName($config->app->shop_host);
 
-            $router->add(
-                "/:controller/:action",
-                [
-                    "module" => "shop",
-                    "controller" => 1,
-                    "action" => 2,
-                ]
-            )->setHostName($config->app->shop_host);
+        $router->add(
+            "/:controller/:action",
+            [
+                "module"     => "shop",
+                "controller" => 1,
+                "action"     => 2,
+            ]
+        )->setHostName($config->app->shop_host);
 
-            // 添加新路由，多一个自定义参数
-            $router->add(
-                "/:controller/:action/:params",
-                [
-                    "module" => "shop",
-                    "controller" => 1,
-                    "action" => 2,
-                    "params" => 3,
-                ]
-            )->setHostName($config->app->shop_host);
+        // 添加新路由，多一个自定义参数
+        $router->add(
+            "/:controller/:action/:params",
+            [
+                "module"     => "shop",
+                "controller" => 1,
+                "action"     => 2,
+                "params"     => 3,
+            ]
+        )->setHostName($config->app->shop_host);
 
-            $router->add(
-                "/common/systemlanguage",
-                [
-                    "module" => "home",
-                    "controller" => "common",
-                    "action" => "systemlanguage"
-                ]
-            )->setHostName($config->app->shop_host);
+        $router->add(
+            "/common/systemlanguage",
+            [
+                "module"     => "home",
+                "controller" => "common",
+                "action"     => "systemlanguage",
+            ]
+        )->setHostName($config->app->shop_host);
 
-            $router->add(
-                "/common/loadname",
-                [
-                    "module" => "home",
-                    "controller" => "common",
-                    "action" => "loadname"
-                ]
-            )->setHostName($config->app->shop_host);
+        $router->add(
+            "/common/loadname",
+            [
+                "module"     => "home",
+                "controller" => "common",
+                "action"     => "loadname",
+            ]
+        )->setHostName($config->app->shop_host);
 
-            $router->add(
-                "/l/([a-z]+)",
-                [
-                    "module" => "home",
-                    "controller" => "common",
-                    "action" => "list",
-                    "table" => 1
-                ]
-            )->setHostName($config->app->shop_host);
+        $router->add(
+            "/l/([a-z]+)",
+            [
+                "module"     => "home",
+                "controller" => "common",
+                "action"     => "list",
+                "table"      => 1,
+            ]
+        )->setHostName($config->app->shop_host);
 
-            return $router;
-        }
+        return $router;
+    }
     );
 
     // Handle the request
@@ -213,18 +210,18 @@ try {
     // 注册模块
     $application->registerModules(
         [
-            "home" => [
+            "home"  => [
                 "className" => "Multiple\\Home\\Module",
-                "path" => "../app/home/Module.php",
+                "path"      => "../app/home/Module.php",
             ],
             "admin" => [
                 "className" => "Multiple\\Admin\\Module",
-                "path" => "../app/admin/Module.php",
+                "path"      => "../app/admin/Module.php",
             ],
-            "shop" => [
+            "shop"  => [
                 "className" => "Multiple\\Shop\\Module",
-                "path" => "../app/shop/Module.php",
-            ]
+                "path"      => "../app/shop/Module.php",
+            ],
         ]
     );
 
